c9fa7f08e7320cc882335fd5988a179280ddc421
msk-UX31E <yn.misaki@gmail.com>
2015-11-26 09:17:08 +0900

    automatic registration

--- setup.sh
+++ setup.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+if [ ! -e "~/.vim" ]; then
+  mkdir -p ~/.vim/colors
+  cd /tmp
+  git clone https://github.com/tomasr/molokai
+  mv molokai/colors/molokai.vim ~/.vim/colors/
+fi
+
 ln -sf ~/dotfiles/_vimrc ~/.vimrc
