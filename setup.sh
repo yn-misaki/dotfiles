b986193fe530725f133b7b511a2f3fb6e7cdb230
Misaki Yanai <yanai@sansan.com>
2018-08-10 10:22:43 +0900

    :fire: remove bunlde install from setup script

--- setup.sh
+++ setup.sh
@@ -7,9 +7,4 @@ if [ ! -e "~/.vim" ]; then
   mv molokai/colors/molokai.vim ~/.vim/colors/
 fi
 
-if [ ! -e "~/.vim/bundle" ]; then
-  mkdir -p ~/.vim/bundle
-  git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-fi
-
 ln -sf ~/dotfiles/_vimrc ~/.vimrc
